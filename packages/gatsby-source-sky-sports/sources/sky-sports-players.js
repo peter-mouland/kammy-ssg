3ecd84f6022fbbff3eef285203b3b202d03f029f
Peter Mouland <peter.mouland@gmail.com>
2020-03-11 11:48:57 +0000

    introduce pmap

--- packages/gatsby-source-sky-sports/sources/sky-sports-players.js
+++ packages/gatsby-source-sky-sports/sources/sky-sports-players.js
@@ -1,3 +1,4 @@
+const pMap = require('p-map');
 const fetch = require('../lib/fetch');
 
 const PLAYERS_URL = 'https://fantasyfootball.skysports.com/cache/json_players.json';
@@ -6,13 +7,12 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const getFixtures = (code) => fetch(`https://fantasyfootball.skysports.com/cache/json_player_stats_${code}.json`);
 
 const fetchPlayersFull = async (players) => {
-  // const promises = [players[0]].map(async (player, i) => {
-  const promises = players.map(async (player, i) => {
-    await delay((i * 50));
+  const mapper = async (player) => {
     const fixtures = await getFixtures(player.id);
     return ({...player, ...fixtures});
-  });
-  return Promise.all(promises);
+  };
+  // return pMap([players[0], mapper, {concurrency: 10 });
+  return pMap(players, mapper, { concurrency: 20 });
 };
 
 const fetchPlayers = async () => {
