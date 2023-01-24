dec7f11e495a65955837933ab3c12799731bd79e
Peter Mouland <peter.mouland@gmail.com>
2023-01-24 08:37:44 +0000

    css swap nav-bar bem for module

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import bemHelper from '@kammy/bem';
 import { useCookies } from 'react-cookie';
 
 import appConfig from '../../config/config';
 import NamedLink from '../named-link';
 import Spacer from '../spacer';
-import './nav-bar.css';
+import * as styles from './nav-bar.module.css';
 
-const bem = bemHelper({ block: 'nav' });
-const linkClass = bem('link');
+const linkClass = styles.nav__link;
 
 const AdminLinks = () => {
     const [cookies] = useCookies(['is-admin']);
     const isAdmin = cookies['is-admin'] === 'true' || false;
     return isAdmin ? (
-        <div className={bem('link', 'right')}>
+        <div>
             <NamedLink to="admin" className={linkClass} />
         </div>
     ) : null;
 };
 
 const Index = () => (
-    <nav className={bem()}>
-        <div className={bem('content')}>
+    <nav className={styles.nav}>
+        <div className={styles.nav__content}>
             <Spacer all={{ right: Spacer.spacings.TINY }} tag="span">
                 <NamedLink to="homepage" />
             </Spacer>
