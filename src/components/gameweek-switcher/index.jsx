f9b3502ebe924275b6876b89dd02236c90ffec9f
Peter Mouland <peter.mouland@gmail.com>
2023-09-10 10:46:27 +0100

    tidyup gw switcher

--- src/components/gameweek-switcher/index.jsx
+++ src/components/gameweek-switcher/index.jsx
@@ -9,17 +9,16 @@ import useGameWeeks from '../../hooks/use-game-weeks';
 
 const GameWeekSwitcher = ({ to, selectedGameWeek }) => {
     const [showSwitcher, toggleSwitcher] = React.useState(false);
-    const { currentGameWeek = {}, gameWeeks } = useGameWeeks();
-    console.log({ gameWeeks, selectedGameWeek });
+    const { gameWeeks } = useGameWeeks();
     const route = findRoute({ to });
     return (
         <span style={{ position: 'relative', zIndex: 10 }}>
             <button
-                style={{ display: 'inline-block', fontSize: '1.3em', padding: 0 }}
+                style={{ display: 'inline-block', padding: 0 }}
                 onClick={() => (to ? toggleSwitcher(!showSwitcher) : null)}
                 type="button"
             >
-                <FormattedGameWeekDate gameWeek={gameWeeks[selectedGameWeek]} isCurrent isSelected />
+                <FormattedGameWeekDate gameWeek={gameWeeks[selectedGameWeek]} isSelected />
             </button>
             {showSwitcher ? (
                 <div
@@ -30,18 +29,15 @@ const GameWeekSwitcher = ({ to, selectedGameWeek }) => {
                         maxHeight: '150px',
                         overflowY: 'scroll',
                         background: 'white',
+                        whiteSpace: 'nowrap',
                         boxShadow:
                             'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset',
                     }}
                 >
-                    {gameWeeks.map(({ id, isCurrent }) => (
-                        <div style={{ margin: '0.15em 0' }} key={id}>
-                            <Link to={`/week-${id}${route.path}`} className="">
-                                <FormattedGameWeekDate
-                                    isSelected={selectedGameWeek === id}
-                                    gameWeek={gameWeeks[id]}
-                                    isCurrent={isCurrent}
-                                />
+                    {gameWeeks.map(({ id }) => (
+                        <div style={{ margin: '0.15em 0', fontSize: '0.7em' }} key={id}>
+                            <Link to={`/week-${id}${route.path}`}>
+                                <FormattedGameWeekDate isSelected={selectedGameWeek === id} gameWeek={gameWeeks[id]} />
                             </Link>
                         </div>
                     ))}
