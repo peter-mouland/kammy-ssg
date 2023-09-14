d426c84e0a1ce9c3f384270705f79331f6bb1417
Peter Mouland <peter.mouland@gmail.com>
2023-09-14 11:28:04 +0100

    minor tweaks

--- src/templates/division-teams.jsx
+++ src/templates/division-teams.jsx
@@ -9,7 +9,6 @@ import * as StatsTable from '../components/division-teams/division-stats.table';
 import { Stats } from '../models/stats';
 import CSquads from '../models/squads';
 import * as Player from '../components/player';
-import * as styles from '../components/division-teams/division-stats.module.css';
 import NavBar from '../components/nav-bar';
 import useAdmin from '../hooks/use-admin';
 import useDivisions from '../hooks/use-divisions';
@@ -17,15 +16,10 @@ import useManagers from '../hooks/use-managers';
 
 const Index = ({ data, pageContext: { gameWeekIndex, divisionId } }) => {
     const { isAdmin } = useAdmin();
-
-    const {
-        currentTeams: { group: currentTeams },
-    } = data;
-
     const Divisions = useDivisions();
     const Managers = useManagers();
     const Division = Divisions.byId[divisionId];
-    const Squads = new CSquads(currentTeams);
+    const Squads = new CSquads(data.currentTeams.group);
     const StatsList = new Stats();
     return (
         <Layout.Container title={`${Division.label} - Teams`}>
@@ -42,64 +36,59 @@ const Index = ({ data, pageContext: { gameWeekIndex, divisionId } }) => {
                 <Layout.Title>Teams</Layout.Title>
                 {isAdmin && <TeamWarnings warnings={Squads.warnings} />}
 
-                <div data-b-layout="vpad" style={{ margin: '0 auto', width: '100%' }}>
-                    {Squads.all.map((Squad) => (
-                        <React.Fragment key={Squad.managerId}>
-                            <StatsTable.Title>{Managers.byId[Squad.managerId].label}</StatsTable.Title>
-                            <StatsTable.Table>
-                                <StatsTable.Thead>
-                                    <StatsTable.Th>Player</StatsTable.Th>
-                                    <StatsTable.Th>Points</StatsTable.Th>
-                                    {/* remove first item, 'points', so we can make it bold, above*/}
-                                    {StatsList.all.slice(1).map((Stat) => (
-                                        <StatsTable.Th desktopOnly key={Stat.id}>
-                                            {Stat.label}
-                                        </StatsTable.Th>
-                                    ))}
-                                    <StatsTable.Th separator>Next Gw</StatsTable.Th>
-                                </StatsTable.Thead>
-                                <StatsTable.Tbody>
-                                    {Squad.players.map((SquadPlayer) => (
-                                        <StatsTable.Tr
-                                            key={SquadPlayer.code}
-                                            className="cell cell--player"
-                                            hasChanged={SquadPlayer.hasChanged}
-                                            hasWarning={
-                                                isAdmin && SquadPlayer.hasWarnings(Squad.warnings, Squads.warnings)
-                                            }
-                                        >
-                                            <StatsTable.Td>
-                                                <Player.AllInfo player={SquadPlayer} />
-                                            </StatsTable.Td>
-                                            <StatsTable.Td>
-                                                <strong>{SquadPlayer.gameWeekStats.points.value}</strong>
-                                            </StatsTable.Td>
+                {Squads.all.map((Squad) => (
+                    <React.Fragment key={Squad.managerId}>
+                        <StatsTable.Title>{Managers.byId[Squad.managerId].label}</StatsTable.Title>
+                        <StatsTable.Table>
+                            <StatsTable.Thead>
+                                <StatsTable.Th>Player</StatsTable.Th>
+                                <StatsTable.Th>Points</StatsTable.Th>
+                                {/* remove first item, 'points', so we can make it bold, above*/}
+                                {StatsList.all.slice(1).map((Stat) => (
+                                    <StatsTable.Th desktopOnly key={Stat.id}>
+                                        {Stat.label}
+                                    </StatsTable.Th>
+                                ))}
+                                <StatsTable.Th separator>Next Gw</StatsTable.Th>
+                            </StatsTable.Thead>
+                            <StatsTable.Tbody>
+                                {Squad.players.map((SquadPlayer) => (
+                                    <StatsTable.Tr
+                                        key={SquadPlayer.code}
+                                        hasChanged={SquadPlayer.hasChanged}
+                                        hasWarning={isAdmin && SquadPlayer.hasWarnings(Squad.warnings, Squads.warnings)}
+                                    >
+                                        <StatsTable.Td>
+                                            <Player.AllInfo player={SquadPlayer} />
+                                        </StatsTable.Td>
+                                        <StatsTable.Td>
+                                            <strong>{SquadPlayer.gameWeekStats.points.value}</strong>
+                                        </StatsTable.Td>
 
-                                            {/* remove first item, 'points', so we can make it bold, above*/}
-                                            {StatsList.all.slice(1).map((Stat) => (
-                                                <StatsTable.Td key={Stat.id} desktopOnly>
-                                                    {SquadPlayer.gameWeekStats[Stat.id].value}
-                                                </StatsTable.Td>
-                                            ))}
-                                            <StatsTable.Td separator>
-                                                <em>
-                                                    {SquadPlayer.nextGameWeekFixtures.map((f) => (
-                                                        <React.Fragment key={f.fixture_id}>
-                                                            {f.oponent.club}{' '}
-                                                            <span className={styles.small}>
-                                                                ({f.oponent.awayOrHomeLabel})
-                                                            </span>
-                                                        </React.Fragment>
-                                                    ))}
-                                                </em>
+                                        {/* remove first item, 'points', so we can make it bold, above*/}
+                                        {StatsList.all.slice(1).map((Stat) => (
+                                            <StatsTable.Td key={Stat.id} desktopOnly>
+                                                {SquadPlayer.gameWeekStats[Stat.id].value}
                                             </StatsTable.Td>
-                                        </StatsTable.Tr>
-                                    ))}
-                                </StatsTable.Tbody>
-                            </StatsTable.Table>
-                        </React.Fragment>
-                    ))}
-                </div>
+                                        ))}
+                                        <StatsTable.Td separator>
+                                            <em>
+                                                {SquadPlayer.nextGameWeekFixtures.map((f) => (
+                                                    <React.Fragment key={f.fixture_id}>
+                                                        {f.oponent.club}{' '}
+                                                        <span style={{ fontSize: '0.8em', color: 'grey' }}>
+                                                            ({f.oponent.awayOrHomeLabel})
+                                                        </span>
+                                                    </React.Fragment>
+                                                ))}
+                                            </em>
+                                        </StatsTable.Td>
+                                    </StatsTable.Tr>
+                                ))}
+                            </StatsTable.Tbody>
+                        </StatsTable.Table>
+                    </React.Fragment>
+                ))}
             </Layout.Body>
         </Layout.Container>
     );
