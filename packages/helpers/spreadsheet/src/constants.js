771905782e18825cc60331511804ff355018f5c1
Peter Mouland <peter.mouland@gmail.com>
2024-08-12 18:13:44 +0100

    revert transfers id

--- packages/helpers/spreadsheet/src/constants.js
+++ packages/helpers/spreadsheet/src/constants.js
@@ -2,8 +2,7 @@ module.exports = {
     spreadsheets: {
         // also /kammy-ssg/packages/gatsby-source-fpl/lib/constants.js
         ACCESS_KEY: 'AIzaSyDA7UXEi96G7qFNIdgMg6nVJW9OajORO-I', // key to get json
-        TRANSFERS_ID: '1zbSCg1yCyRkAw6jsQEXlpeK1bk1VpNbl5gR4_D1dX5Y', // prod transfers
-        // TRANSFERS_ID: '1YsVfFHVvCasF6J4wDargP67_rY51MNO9Y7584wEeeLQ', // dev transfers
+        TRANSFERS_ID: '1sU3xKfpOD6m3hdYqddfMXpVmvkJu4_LdUnJd1YLzU9U', // prod transfers
         SETUP_ID: '14UJbIM10PAIvL_9pH1vkC8sIzwWRbIqUbBNEoZTVYuQ', // game-weeks, players, teams + draft picks
     },
 };
